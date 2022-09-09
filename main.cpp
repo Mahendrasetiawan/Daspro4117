20fa3cd4370fa0854d119930e324e91f74146b64
Mahendrasetiawan <112888273+Mahendrasetiawan@users.noreply.github.com>
2022-09-09 17:15:20 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Mahendra Setiawan" << endl;
+    cout << "A11.2022.14638" << endl;
+    cout << "Laki-Laki" << endl;
+    cout << "SMK HARAPAN MULYA" << endl;
+    return 0;
+}
