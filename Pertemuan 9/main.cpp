68e8197a53d08e454127f504fa4a0a8790818127
Mahendrasetiawan <112888273+Mahendrasetiawan@users.noreply.github.com>
2022-10-08 18:44:05 +0700

    Add files via upload

---
+++ Pertemuan 9/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 5;
+
+    cout <<"Tugas daspro pertemuan 9" << endl;
+    cout <<"Mencari angka kelipatan 5" << endl;
+
+    for (x = 5; x <= 100; x = x+5) {
+        cout <<  x  << " " ;
+    }
+    return 0;
+}
